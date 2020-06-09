d661fa3a7ca16c6758fc1d521850b44d2a16a979
@skruru <s14k_ex-fw-myt@ezweb.ne.jp>
2020-06-10 02:46:35 +0900

    fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-const num = 7;
+let a = '文字１';
+let b = a;
+a = '文字２';
+console.log(a);
+console.log(b);
 
-console.log('足し算');
-console.log(num + 1000);
+let objectA = {
+    name: '名前１',
+    age:20,
+  };
+  let objectB = objectA;
+  objectA.age = 30;
+  console.log(objectA);
+  console.log(objectB);
